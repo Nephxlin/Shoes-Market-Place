ecff619da096ed5a371e524b7ccdaeb268285d62
Luther <luther.m.cordeiro@gmail.com>
2020-09-13 13:05:22 -0300

    Repair Routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Cart from './pages/Cart';
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 export default function Routes() {
-  return(
+  return (
     <Switch>
-    <Route Path="/" exact component={Home} />
-    <Route Path="/Cart" component={Cart} />
-  </Switch>
-  )
+      <Route path="/" exact component={Home} />
+      <Route path="/cart" component={Cart} />
+    </Switch>
+  );
 }
