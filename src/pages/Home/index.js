8de2f6bff2428f9039ba9bfb26e7dcce7b3e908c
Luther <luther.m.cordeiro@gmail.com>
2020-09-13 10:09:34 -0300

    First Commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {ProductList} from './styles'
+
+export default function Home (){
+  return(
+    <ProductList>
+      <li>
+        <img src="" alt=""/>
+      </li>
+    </ProductList>
+  )
+}
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route Path="/" exact component={Home} />
+      <Route Path="/Cart" component={Cart} />
+    </Switch>
+  )
+}
