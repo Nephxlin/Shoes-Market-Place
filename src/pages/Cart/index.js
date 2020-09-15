898cfb1a1267fcd2a1d1ae2286e116cff09a4e7a
Luther <luther.m.cordeiro@gmail.com>
2020-09-14 22:40:13 -0300

    feat Redux Saga- Add Cart item limit

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators} from 'redux'
+
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
   MdDelete,
 } from "react-icons/md";
 
+import {formatPrice} from '../../util/format'
+
+import * as CartActions from '../../store/modules/cart/actions'
+
 import { Container, ProductTable, Total } from "./styles";
 
-export default function Cart() {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
+  function increment(product){
+    updateAmountRequest(product.id, product.amount +1)
+  }
+
+  function decrement(product){
+    updateAmountRequest(product.id, product.amount -1)
+  }
+
   return (
     <Container>
       <ProductTable>
@@ -21,41 +36,43 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-20-masculino/26/NQQ-0144-026/NQQ-0144-026_detalhe1.jpg?ts=1581718228?ims=280x280"
-                alt="tenis"
-              />
-            </td>
+          {cart.map(product => (
+            <tr key={product.id}>
+              <td>
+                <img
+                  src={product.image}
+                  alt={product.title}
+                />
+              </td>
 
-            <td>
-              <strong>Tenis muito massa</strong>
-              <span>129,90</span>
-            </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
 
-            <td>
-              <div>
-                <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+              <td>
+                <div>
+                  <button type="button" onClick={() => decrement(product)}>
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount}/>
+                  <button type="button" onClick={() => increment(product)}>
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+
+              <td>
+                <strong>{product.subtotal}</strong>
+              </td>
+
+              <td>
+                <button type="button" onClick={() => removeFromCart(product.id)}>
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input type="number" readOnly value={1} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-
-            <td>
-              <strong>R$258,80</strong>
-            </td>
-
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
 
@@ -64,9 +81,26 @@ export default function Cart() {
 
         <Total>
           <span>Total</span>
-          <strong>R$1290,00</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
   );
 }
+
+const mapStateToProps = (state) => ({
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+  }, 0)),
+});
+
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators( CartActions,dispatch)
+
+export default connect(mapStateToProps,mapDispatchToProps)(Cart);
